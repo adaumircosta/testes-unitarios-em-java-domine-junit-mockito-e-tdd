9f72c3f2145aa016257b61f1cad36d90972d5540
Adaumir Costa <adaumircosta@gmail.com>
2020-01-20 21:47:14 -0300

    Ordem de execução dos testes

---
+++ src/test/java/br/ce/wcaquino/servicos/OrdemTeste.java
@@ -0,0 +1,23 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class OrdemTeste {
+	
+	public static int contador = 0;
+
+	@Test
+	public void inicia() {
+		contador = 1;
+	}
+
+	@Test
+	public void verifica() {
+		Assert.assertEquals(1, contador);
+	}
+ 
+}
